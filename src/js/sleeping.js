ae980c0e19600caa60d452e6b9d0a50c3eb4b23c
Hobotnitsa <dariagarnik@gmail.com>
2023-08-07 23:55:48 +0300

    change syntax of getters and setters

--- src/js/sleeping.js
+++ src/js/sleeping.js
@@ -1,66 +1,57 @@
 export class Sleeping {
-    #startTime;
-    #startTimeDiff;
-    #endTime;
-    #endTimeDiff;
-    #recommendations;
-
     constructor(startTime, startTimeDiff, endTime, endTimeDiff, recommendations) {
-        this.#startTime = startTime;
-        this.#startTimeDiff = startTimeDiff;
-        this.#endTime = endTime;
-        this.#endTimeDiff = endTimeDiff;
-        this.#recommendations = recommendations;
-    }
-
-    getStartTime() {
-        return this.#startTime;
-    }
-
-    setStartTime(value) {
-        this.#startTime = value;
-    }
-
-    getStartTimeDiff() {
-        return this.#startTimeDiff;
-    }
-
-    setStartTimeDiff(value) {
-        this.#startTimeDiff = value;
-    }
-
-    getEndTime() {
-        return this.#endTime;
-    }
-
-    setEndTime(value) {
-        this.#endTime = value;
-    }
-
-    getEndTimeDiff() {
-        return this.#endTimeDiff;
-    }
-
-    setEndTimeDiff(value) {
-        this.#endTimeDiff = value;
-    }
-
-    getRecommendations() {
-        return this.#recommendations;
-    }
-
-    setRecommendations(value) {
-        this.#setRecommendations = value;
-    }
-
-    getSleepDuration() {
-        return new Date(this.#endTime.getTime() - this.#startTime.getTime());
+        this.startTime = startTime;
+        this.startTimeDiff = startTimeDiff;
+        this.endTime = endTime;
+        this.endTimeDiff = endTimeDiff;
+        this.recommendations = recommendations;
     }
 }
+
+Sleeping.prototype.getStartTime = function () {
+    return this.startTime;
+};
+
+Sleeping.prototype.setStartTime = function (value) {
+    this.startTime = value;
+};
+
+Sleeping.prototype.getStartTimeDiff = function () {
+    return this.startTimeDiff;
+};
+
+Sleeping.prototype.setStartTimeDiff = function (value) {
+    this.startTimeDiff = value;
+};
+
+Sleeping.prototype.getEndTime = function () {
+    return this.endTime;
+};
+
+Sleeping.prototype.setEndTime = function (value) {
+    this.endTime = value;
+};
+
+Sleeping.prototype.getEndTimeDiff = function () {
+    return this.endTimeDiff;
+};
+
+Sleeping.prototype.setEndTimeDiff = function (value) {
+    this.endTimeDiff = value;
+};
+
+Sleeping.prototype.getRecommendations = function () {
+    return this.recommendations;
+};
+
+Sleeping.prototype.setRecommendations = function (value) {
+    this.setRecommendations = value;
+};
+
+Sleeping.prototype.getSleepDuration = function () {
+    return new Date(this.endTime.getTime() - this.startTime.getTime());
+};
 
 export class DayNap extends Sleeping {}
 
-export class NightSleep extends Sleeping {
-    #sleepDurBeforeMid;
-    #sleepDurAfterMid;
-}
+export class NightSleep extends Sleeping {}
