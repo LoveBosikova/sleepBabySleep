00e967bef4318fd4c3380161257b805393163909
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-08-07 16:59:13 +0300

    class Day

---
+++ src/js/Day.js
@@ -0,0 +1,24 @@
+class Day {
+    constructor (data) {
+        this.data = data;
+        this.dayNaps = [];
+        this.nightSleep = [];
+        this.bestMatch;
+    }
+}
+
+Day.prototype.getData = function () {
+    return this.data;
+}
+
+Day.prototype.dayNaps = function () {
+    return this.dayNaps;
+}
+
+Day.prototype.nighthtSleep = function () {
+    return this.nighthtSleep;
+}
+
+Day.prototype.nightSleep = function () {
+    return this.nighthtSleep;
+}
