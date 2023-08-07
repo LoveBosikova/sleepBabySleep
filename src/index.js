364cef928daaabf1f32ddccfbfdd6865f941c9c4
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-08-08 01:05:20 +0300

    file str improvement

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-// Наши главные файлы - в сасс можно работать с js, плюс подключены момент и чарт
-import './sass/main.scss';
-import moment from 'moment';
-import Chart from 'chart.js/auto';
-import Day from './js/Day.js';
 
-// Примеры импортов кода и картинок
-import tesla from './assets/picture.jpg';
-import hello from'./js/hello';
+import app from './js/hello'
 
-
+app();
---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+// Наши главные файлы - в сасс можно работать с js, плюс подключены момент и чарт
+import './sass/main.scss';
+import moment from 'moment';
+import Chart from 'chart.js/auto';
+import Day from './js/Day.js';
+
+// Примеры импортов кода и картинок
+import tesla from './assets/picture.jpg';
+import hello from'./js/hello';
+
+const app = () => {
+
+}
+
+export default app;
