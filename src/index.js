ac3a90b81005beb4c20844248059466397b6ed06
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-08-07 17:05:09 +0300

    class Day imported in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // Наши главные файлы - в сасс можно работать с js, плюс подключены момент и чарт
-import './sass/main.scss'
-import moment from 'moment'
-import Chart from 'chart.js/auto'
+import './sass/main.scss';
+import moment from 'moment';
+import Chart from 'chart.js/auto';
+import Day from './js/Day.js';
 
 // Примеры импортов кода и картинок
-import tesla from './assets/picture.jpg'
-import hello from'./js/hello'
+import tesla from './assets/picture.jpg';
+import hello from'./js/hello';
 
 
