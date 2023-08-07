1ddc4c171c6301a5ad433f1a220f73e9b2e4d7bf
Daria <d.a.kireeva@mail.ru>
2023-08-07 17:36:24 +0300

    create class Baby

---
+++ src/js/baby.js
@@ -0,0 +1,39 @@
+class Baby {
+    constructor(name, mother, father, birthday, temperament) {
+        this.name = name;
+        this.mother = mother;
+        this.father = father;
+        this.birthday = birthday;
+        this.temperament = temperament;
+    }
+    getImage() {
+        return {
+            image: this.image
+        };
+    }
+    getAge() {
+        let todayYear = new Date().getFullYear();
+        let todayMonth = new Date().getMonth();
+        let todayDay = new Date().getDate();
+
+        return {
+            year: todayYear - this.birthday.getFullYear(),
+            month: todayMonth - this.birthday.getMonth(),
+            day: todayDay - this.birthday.getDate(),
+        }
+    }
+    getDaytimeSleep(newTime) {
+        return daySleeps.push(newTime);
+    }
+    getNightSleep(newTime) {
+        return nightSleeps.push(newTime);
+    }
+    getBetterDaySpeep() {
+        let max = daySleeps.sort((a, b) => b - a)[0];
+        return max;
+    }
+    getBetterNightSpeep() {
+        let max = nightSleeps.sort((a, b) => b - a)[0];
+        return max;
+    }
+}
