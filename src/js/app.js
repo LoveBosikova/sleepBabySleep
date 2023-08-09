e9bef9a8800f47c995b445e4a208e2c7be45ca4b
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-08-09 16:19:33 +0300

    watcher added, onchange added, fnc render added

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 // Наши главные файлы - в сасс можно работать с js, плюс подключены момент и чарт
 import './sass/main.scss';
 import moment from 'moment';
+import onChange from 'on-change';
 import Chart from 'chart.js/auto';
 import Day from './js/Day.js';
+import render from './view.js';
 
 // Примеры импортов кода и картинок
 import tesla from './assets/picture.jpg';
@@ -12,7 +14,7 @@ const app = () => {
 
     const fullformat = 'DD.MM.YYYY';
 
-    //Комплексное состояние приложения
+    //Комплексное состояние приложения. Здесь всё, что влияет на отображение объектов на странице
     const state = {
         registrationForm: {
             valid: false,
@@ -22,6 +24,12 @@ const app = () => {
             },
             },
     }
+
+    // Это наш вотчерю он смотрит за любыми изменениями, которые происходят в состоянии
+    // Первый параметр - за каким объектом следим, второй - какую функцию запускаем
+    const watchedState = onChange(state, render);
+    // Когда страница будет грузится, состояние отобразится начальное (плюс то, которое зависит от локальных хранилищ данных)
+    render(state);
 
 }
 
