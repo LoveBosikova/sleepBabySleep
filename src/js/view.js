f382571e2ca553d10bc1d98c50e6d4e31f084883
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-08-11 17:11:17 +0300

    rewrite mistakes

--- src/js/view.js
+++ src/js/view.js
@@ -1,15 +1,15 @@
 const render = (state) => {
     // Это пример
-    const submit = document.querySelector(/* .... */);
-    const input = document.querySelector(/* .... */);
+    // const submit = document.querySelector(/* .... */);
+    // const input = document.querySelector(/* .... */);
 
-    submit.disabled = !state.registrationForm.valid;
+    // submit.disabled = !state.registrationForm.valid;
 
-    if (state.registrationForm.valid) {
-        input.style.border = null;
-    } else {
-        input.style.border = 'thick solid red';
-    }
+    // if (state.registrationForm.valid) {
+    //     input.style.border = null;
+    // } else {
+    //     input.style.border = 'thick solid red';
+    // }
 };
 
 export default render;
