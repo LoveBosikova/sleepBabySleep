6dee9bb546997f6f1743d8357aa1b73b46b05c29
Daria <d.a.kireeva@mail.ru>
2023-08-16 16:02:30 +0300

    добавила файлы для функций

---
+++ src/js/changeVisibility.js
@@ -0,0 +1,15 @@
+// function changeVisibility(i) {
+//     switch (state[i].display) {
+//         case 'block':
+//             state[i].display = 'none';
+//             break;
+//         case 'none':
+//             state[i].display = 'block';
+//             break;
+//     }
+//     const faqList = document.getElementById('faqList');
+//     faqList.innerHTML = '';
+//     createFaq(state);
+// }
+
+// export default changeVisibility;
