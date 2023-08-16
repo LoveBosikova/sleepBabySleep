0da3ef2a86c46c869d79ae96e2badd5960ea7c40
Daria <d.a.kireeva@mail.ru>
2023-08-16 20:07:07 +0300

    добавила функцию по новой отрисовке элементов

--- src/js/view.js
+++ src/js/view.js
@@ -1,11 +1,16 @@
 import createDays from './createDays.js';
 import renderTags from './renderTags.js';
+import createFaq from './createFAQ.js';
 
 const render = (state) => {
     const daysWrap = document.getElementById('datesWrap');
     daysWrap.innerHTML = '';
     createDays(state.stateUI.calendarWeek);
     renderTags(state.stateUI.tags);
+
+    const faqList = document.getElementById('faqList');
+    faqList.innerHTML = '';
+    createFaq(state.stateUI.accordion)
 };
 
 export default render;
