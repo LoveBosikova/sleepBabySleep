76860e28933f51687dc00817d217c0db40603aaf
Daria <d.a.kireeva@mail.ru>
2023-08-16 16:01:31 +0300

    добавила массив для аккордеона

--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,12 @@ import onChange from 'on-change';
 import Chart from 'chart.js/auto';
 import render from './view.js';
 import createDays from './createDays.js'
-import { dayForward, dayBackward } from './changeDay.js'
+import {
+    dayForward,
+    dayBackward
+} from './changeDay.js'
 import renderTags from './renderTags.js';
+import faq from './faq.js';
 
 // Примеры импортов кода и картинок
 
@@ -15,6 +19,15 @@ const app = async () => {
     const fullformat = 'DD.MM.YYYY';
     const btnDayForward = document.getElementById('btn-dayForward');
     const btnDayBackward = document.getElementById('btn-dayBackward');
+
+    const faqAcc = Array.from(faq.map((item) => {
+        return {
+            faqId: item.id,
+            question: item.question,
+            answer: item.answer,
+            display: 'none',
+        }
+    }));
 
     //Комплексное состояние приложения. Здесь всё, что влияет на отображение объектов на странице
     const state = {
@@ -26,7 +39,7 @@ const app = async () => {
             },
         },
         stateUI: {
-            calendarWeek:[
+            calendarWeek: [
                 [moment().day(-3).format('ddd'), moment().day(-3).format('DD'), moment().day(-3)],
                 [moment().day(-2).format('ddd'), moment().day(-2).format('DD'), moment().day(-2)],
                 [moment().day(-1).format('ddd'), moment().day(-1).format('DD'), moment().day(-1)],
@@ -35,32 +48,60 @@ const app = async () => {
                 [moment().day(2).format('ddd'), moment().day(2).format('DD'), moment().day(2)],
                 [moment().day(3).format('ddd'), moment().day(3).format('DD'), moment().day(3)],
             ],
-            tags: [
-                { value: 'nipple', text: 'Соска', isChecked: false},
-                { value: 'bottle', text: 'Бутылочка', isChecked: false},
-                { value: 'sleepHisteria', text: 'Заснул с истерикой', isChecked: false},
-                { value: 'perfectSleeping', text: 'Хорошо заснул', isChecked: false},
-                { value: 'weakingUpHisteria', text: 'Проснулся с истерикой', isChecked: false},
-                { value: 'perfectWeakingUp', text: 'Проснулся в настроении', isChecked: false}
-            ]
+            tags: [{
+                    value: 'nipple',
+                    text: 'Соска',
+                    isChecked: false
+                },
+                {
+                    value: 'bottle',
+                    text: 'Бутылочка',
+                    isChecked: false
+                },
+                {
+                    value: 'sleepHisteria',
+                    text: 'Заснул с истерикой',
+                    isChecked: false
+                },
+                {
+                    value: 'perfectSleeping',
+                    text: 'Хорошо заснул',
+                    isChecked: false
+                },
+                {
+                    value: 'weakingUpHisteria',
+                    text: 'Проснулся с истерикой',
+                    isChecked: false
+                },
+                {
+                    value: 'perfectWeakingUp',
+                    text: 'Проснулся в настроении',
+                    isChecked: false
+                }
+            ],
+            accordion: faqAcc,
         }
     }
-    
+
     // Обработчики событий ничего не меняют во внешнем виде приложения, они меняют состояние. 
     // А уже функция рендеринга обрабатывает состояние и меняет внешний вид
 
-    btnDayForward.addEventListener('click', ()=> { dayForward(state)});
-    btnDayBackward.addEventListener('click', ()=> { dayBackward(state)});
+    btnDayForward.addEventListener('click', () => {
+        dayForward(state)
+    });
+    btnDayBackward.addEventListener('click', () => {
+        dayBackward(state)
+    });
 
     // Когда страница будет грузится, состояние отобразится начальное (плюс то, которое зависит от локальных хранилищ данных)
     render(state);
-    
+
     const tags = document.querySelectorAll('.calendar__tagLabel');
 
     for (const tag of tags) {
         console.log(tag);
         tag.addEventListener('click', (e) => {
-            for(const item of state.stateUI.tags){
+            for (const item of state.stateUI.tags) {
                 if (item.value == e.target.previousElementSibling.value) {
                     item.isChecked = item.isChecked == false ? true : false;
                 }
@@ -69,5 +110,6 @@ const app = async () => {
         })
     }
 }
+
 
 export default app;
