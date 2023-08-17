87a3f74cc833c02ebe56e1f15a5d1b081d24dad6
Hobotnitsa <dariagarnik@gmail.com>
2023-08-17 13:32:37 +0300

    add registration form

---
+++ src/js/registration_form.js
@@ -0,0 +1,22 @@
+window.addEventListener('load', () => {
+    const openRegForm = document.querySelector('.header__sign_in');
+    const modalRegForm = document.querySelector('.registration-form__modal');
+    const closeRegForm = document.querySelector('.registration-form__close-icon');
+
+    openRegForm.addEventListener('click', () => {
+        modalRegForm.style.display = 'flex';
+    });
+
+    closeRegForm.addEventListener('click', () => {
+        modalRegForm.style.display = 'none';
+    });
+
+    ['photoBaby', 'photoDad', 'photoMom'].forEach((inputName) => {
+        document.forms.registrationForm.elements[inputName].addEventListener('change', function () {
+            if (!this.files.length) {
+                return;
+            }
+            this.parentElement.querySelector('span').textContent = this.files[0].name;
+        });
+    });
+});
