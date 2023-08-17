61f50e8b317cad43e6bfd6b30ac09d7d39d71936
Daria <d.a.kireeva@mail.ru>
2023-08-17 11:50:42 +0300

    добавила изменение стилей

--- src/js/changeVisibility.js
+++ src/js/changeVisibility.js
@@ -1,11 +1,29 @@
-function changeVisibility(state, i) {
-    if (state.display == 'none') {
-        state.display = 'block';
-    } else {
-        state.display = 'none';
-    }
-    const answers = [...document.getElementsByClassName('accordion__answer')];
-    answers[i].style.display = state.display;
+function changeVisibility(state) {
+    state.forEach((el, i) => {
+        const questions = [...document.getElementsByClassName('accordion__question-title')];
+        const content = [...document.getElementsByClassName('accordion__question-content')];
+        const answers = [...document.getElementsByClassName('accordion__answer')];
+        const items = [...document.getElementsByClassName('accordion__item')]
+
+        if (el.display == 'none') {
+            content[i].textContent = '+';
+            content[i].style.color = '#FFFFFF';
+            content[i].style.backgroundColor = '#2F80EC';
+            questions[i].style.fontFamily = 'Bellota Text';
+            questions[i].style.fontWeight = '700';
+            items[i].style.backgroundColor = '#10132F';
+        } else {
+            content[i].textContent = '-';
+            content[i].style.color = '#2F80EC';
+            content[i].style.backgroundColor = '#FFFFFF';
+            questions[i].style.fontFamily = 'JetBrains Mono';
+            questions[i].style.fontWeight = '300';
+            items[i].style.backgroundColor = '#2F80EC';
+        }
+
+        answers[i].style.display = el.display;
+    });
+
 }
 
 export default changeVisibility;
