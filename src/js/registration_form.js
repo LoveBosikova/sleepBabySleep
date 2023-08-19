eddf3beba8b6d749bf9652582d626b771ebde570
Hobotnitsa <dariagarnik@gmail.com>
2023-08-19 21:41:50 +0300

    fix, comments

--- src/js/registration_form.js
+++ src/js/registration_form.js
@@ -16,9 +16,9 @@ window.addEventListener('load', () => {
 
     function closeRegForm () {
             modalRegForm.addEventListener('transitionend', function handleTransitionEnd() {
-            modalRegForm.removeEventListener('transitionend', handleTransitionEnd);
-            modalRegForm.style.display = 'none';
-        });
+                modalRegForm.removeEventListener('transitionend', handleTransitionEnd);
+                modalRegForm.style.display = 'none';
+            });
     
         modalRegForm.classList.remove('registration-form__modal-open');
     }
@@ -29,22 +29,23 @@ window.addEventListener('load', () => {
     openRegFormFooter.addEventListener('click', openRegForm);
     regFormContainer.addEventListener('click', (e) => e.stopPropagation());
 
-    ['photoBaby', 'photoDad', 'photoMom'].forEach((inputName) => {
-        form.elements[inputName].addEventListener('change', function () {
-            if (!this.files.length) {
-                return;
-            }
-            this.parentElement.querySelector('.registration-form__photo-comment').textContent = this.files[0].name;
-        });
-    });
+    for(const element of form.elements) {
+        if (element.getAttribute('type') === 'file') {
+            element.addEventListener('change', function() {
+                if (this.files.length) {
+                    this.parentElement.querySelector('.registration-form__photo-comment').textContent = this.files[0].name;
+                }
+            })
+        }
+    }
 
     // коллекция валидаторов
-    // поле validator - имплементация валидатора. Получает текстовое значение, возвращает true если значение корректно, иначе false
+    // поле validator - метод, принимает текстовое значение, возвращает true если значение корректно, иначе false
     // поле message - сообщение об ошибке если поле не валидно
     const validators = {
         required: {
             validator: value => !!value,
-            message: 'Поле обязательно для заполнения!'
+            message: 'Поле обязательно для заполнения'
         },
         name: {
             validator: value => !value || /^[a-zA-Zа-яА-Я][a-zA-Z0-9а-яА-Я ]+$/.test(value),
@@ -52,11 +53,11 @@ window.addEventListener('load', () => {
         },
         email: {
             validator: value => !value || /^[A-Za-z0-9+_.-]+@(.+)$/.test(value),
-            message: 'Неправильный формат электронной почты, например "username@domain.com"'
+            message: 'Введите почту в формате user@domain.com'
         },
         password: {
             validator: value => !value || value.length >= 8,
-            message: 'Пароль должен быть не менее 8 символов длиной'
+            message: 'Пароль должен содержать не менее 8 символов'
         },
         passwordConfirm: {
             validator: value => value === form.elements.passwordRegForm.value,
@@ -67,51 +68,46 @@ window.addEventListener('load', () => {
     submitRegFormBtn.addEventListener('click', (event) => {
         event.preventDefault();
 
-        // пробегаем по всем полям формы, вызывая для каждого валидацию
-        // для каждой итерации возвращаем true если поле валидно и false если нет
-        // form.elements - не массив, а HTMLCollection, у него нет метода map()
-        // поэтому следует пересобрать элементы в массив для дальнейшей потоковой обработки
-        const valid = [...form.elements].map(field => {
-            // получаем назначенные полю валидаторы
-            const validatorNames = field.getAttribute('validators');
+        // пробегаем по всем полям формы, для каждой итерации возвращаем true если поле валидно и false если нет
+        // form.elements - пересобираем элементы в массив для дальнейшей обработки
+        // получаем назначенные полю валидаторы
+
+        const valid = [...form.elements].map(element => {
+            const validatorNames = element.getAttribute('validators');
             if (!validatorNames) {
                 // валидатор не назначен. поле валидно без проверок
                 return true;
             }
 
-            // получаем HTML елемент в котором отображается ошибка для поля, сбрасываем его состояние в дефолтное
-            const errorElement = field.parentElement.querySelector('.registration-form__error');
+            const errorElement = element.parentElement.querySelector('.registration-form__error');
             if (errorElement) {
                 errorElement.textContent = '';
                 errorElement.style.display = 'none';
             }
 
-            // имена валидаторов перечислены через запятую
-            // поэтому прежде всего разбиваем строку по запятым, и затем работаем с отдельными именами валидаторов
+            // имена валидаторов перечислены через запятую, разбиваем
+            // если валидатор не найден, вернется undefined, фильтруем результат чтобы остались только существующие валидаторы
+            // применяем валидатор и заменяем его на результат его работы
+            // возвращаем результат работы валидатора (true / false)
+            // полученный массив результатов валидации объединяем через логическое "и"
+            // результат - true если все результаты массива true, иначе false
+            // если ни один валидатор не был применен (не нашлось валидаторов по заданным именам), вернем true
+            // объединяем результаты валидации всех полей через логическое "и"
+            // в результате будет true если все поля успешно провалидированы, иначе false
+
             return validatorNames.split(',')
-                // подставляем вместо имени валидатора соответствующий блок из validators
                 .map(name => validators[name])
-                // если валидатор не найден, нам вернется undefined. Фильтруем результат чтобы остались только существующие валидаторы
-                .filter(v => !!v)
-                // применяем валидатор и заменяем его на результат его работы
+                .filter(item => !!item)
                 .map(validator => {
-                    // применяем валидатор
-                    const result = validator.validator(field.value);
-                    // выставляем сообщение об ошибке если он вернул false
+                    const result = validator.validator(element.value);
                     if (errorElement && !result) {
-                        errorElement.textContent += '* ' + validator.message;
+                        errorElement.textContent += '*' + validator.message;
                         errorElement.style.display = 'flex';
                     }
-                    // возвращаем результат работы валидатора (true / false)
                     return result;
                 })
-                // полученный массив результатов валидации объединяем через логическое "и"
-                // результат - true если все результаты массива true, иначе false
-                // если ни один валидатор не был применен (не нашлось валидаторов по заданным именам), вернем true
                 .reduce((prev, cur) => prev && cur, true);
         })
-        // объединяем результаты валидации всех полей через логическое "и"
-        // в результате будет true если все поля успешно провалидированы, иначе false
         .reduce((prev, cur) => prev && cur, true);
         
         if (valid) {
