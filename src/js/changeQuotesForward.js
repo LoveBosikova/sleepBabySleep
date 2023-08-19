0feb8a9ed6501d581348e434a491b5e6aa528c3c
Daria <d.a.kireeva@mail.ru>
2023-08-19 11:31:23 +0300

    добавила кнопки переключения цитат

--- src/js/changeQuotesForward.js
+++ src/js/changeQuotesForward.js
@@ -1,15 +1,16 @@
 import quotes from "./quotes";
 let i = 0;
 
-function changeQuotes() {
+function changeQoutesForward() {
     const qoute1 = document.getElementById('quote1');
     const quote2 = document.getElementById('quote2');
-    if (i == quotes.length - 1) {
+    if (i + 1 >= quotes.length - 1) {
         i = 0;
     }
     qoute1.textContent = `${quotes[i].quote}`;
     quote2.textContent = `${quotes[i+1].quote}`;
-    ++i;
+    i += 2;
 }
 
-export default changeQuotes;
+
+export default changeQoutesForward;
