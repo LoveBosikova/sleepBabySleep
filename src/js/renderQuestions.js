482b05d2b0961330678b6e5373d18a13599afc63
Daria <d.a.kireeva@mail.ru>
2023-09-01 17:19:46 +0300

    подправила стили для цитат

--- src/js/renderQuestions.js
+++ src/js/renderQuestions.js
@@ -5,8 +5,6 @@ function renderQuestions(state) {
         const answers = [...document.getElementsByClassName('accordion__answer')];
         const items = [...document.getElementsByClassName('accordion-item')];
 
-        answers[i].style.display = el.display;
-
         if (el.display == 'none') {
             content[i].textContent = '+';
             content[i].style.color = '#FFFFFF';
@@ -14,26 +12,29 @@ function renderQuestions(state) {
             questions[i].style.fontFamily = 'Bellota Text';
             if (questions[i].style.fontWeight != '700') {
                 items[i].style.animation = "itemClose 1s";
+                answers[i].style.animation = "changeOpasityBack 1s";
             }
             questions[i].style.fontWeight = '700';
             items[i].style.backgroundColor = '#10132F';
             items[i].style.paddingBottom = '1em';
+            answers[i].style.opacity = '0';
+            answers[i].style.paddingBottom = '0';
         } else {
             content[i].textContent = '-';
             content[i].style.color = '#2F80EC';
             content[i].style.backgroundColor = '#FFFFFF';
-            questions[i].style.animation = 'answerOpen 1s';
             questions[i].style.fontFamily = 'JetBrains Mono';
             if (questions[i].style.fontWeight != '300') {
                 items[i].style.animation = "itemOpen 1s";
-                answers[i].style.animation = "changeOpasity 1s";
+                answers[i].style.animation = "increaseHeight 2s";
             }
+            answers[i].style.opacity = '1';
+            answers[i].style.height = '100%';
+            answers[i].style.paddingBottom = '1em';
             questions[i].style.fontWeight = '300';
             items[i].style.backgroundColor = '#2F80EC';
             items[i].style.paddingBottom = '2em';
         }
-
-        setTimeout(() => items[i].style.animation = '', 500)
     });
 }
 
